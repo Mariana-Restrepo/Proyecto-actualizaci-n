ee4ecf0d1e8f734cc40d24155ff8e2f0658020fe
practicante analitica 3 <panalit3@crystal.com.co>
2026-05-06 18:24:49 -0500

    Commit inicial con proyecto y GitHub Actions

---
+++ src/assets/components/MiComponente.tsx
@@ -0,0 +1,10 @@
+const MiComponente = () => {
+  return (
+    <div>
+      <h2>Mi aporte individual</h2>
+      <p>Este es mi componente funcional en React.</p>
+    </div>
+  );
+};
+
+export default MiComponente;
