59fd0f6a3abdc6d0532bb396ae373443ef5221ea
Samuel A'v <104322437+SamuAv@users.noreply.github.com>
2026-05-06 17:41:45 -0500

    Add files via upload

---
+++ Proyecto-actualizaci-n-main/src/App.tsx
@@ -0,0 +1,12 @@
+import MiComponente from "./assets/components/MiComponente";
+
+function App() {
+  return (
+    <div>
+      <h1>Proyecto principal</h1>
+      <MiComponente />
+    </div>
+  );
+}
+
+export default App;
---
+++ Proyecto-actualizaci-n-main/src/assets/components/SamuComponente.tsx
@@ -0,0 +1,10 @@
+const SamuComponente = () => {
+    return (
+        <div>
+            <h2>aporte individual de Samuel</h2>
+            <p>Hola Este es mi componente funcional en React.</p>
+        </div>
+    );
+};
+
+export default SamuComponente;
